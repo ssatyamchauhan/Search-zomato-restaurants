ebea96d98cdd63dfcf92986efb92455baf64c6c2
ssatyamchauhan <ssatyamchauhan1998@gmail.com>
2020-01-25 11:42:52 +0530

    added phone number and timing section in app

--- src/Components/zomato.js
+++ src/Components/zomato.js
@@ -1,51 +1,14 @@
 import React from 'react';
-// import { location } from './zomatoapi'
 import axios from 'axios';
 import './zomato.css'
+
 
 export default function Zomato() {
 
     const [state, setState] = React.useState({
         data: [],
-        responseStatus: ''
+        responseStatus: 'Yet You have not Entered Any Location Please Enter location Above to find out the Nearby Zomato Restaurants'
     })
-
-
-    React.useEffect(() => {
-        axios({
-            method: "GET",
-            url: "https://developers.zomato.com/api/v2.1/geocode?lat=28.57005&lon=77.32317",
-            headers: {
-                "user-key": "ff4f897b8bc0d97ccd3ed25a6b951fd3",
-                "content-type": "application/json"
-            }
-        })
-            .then(response => {
-                let Mainlist = []
-                let simplelist = {}
-                let nearbyRest = response.data.nearby_restaurants;
-                for (var i of nearbyRest) {
-                    simplelist.name = (i.restaurant.name)
-                    simplelist.average_cost_for_two = (i.restaurant.average_cost_for_two)
-                    simplelist.has_online_delivery = (i.restaurant.has_online_delivery) ? "Available" : "Not Available"
-                    simplelist.rating = (i.restaurant.user_rating.aggregate_rating)
-                    simplelist.cuisines = (i.restaurant.cuisines).split(',')
-                    simplelist.rating_text = (i.restaurant.user_rating.rating_text)
-                    simplelist.featured_image = i.restaurant.featured_image
-                    simplelist.location = i.restaurant.location.locality
-                    Mainlist.push(simplelist)
-                    simplelist = {}
-                }
-                setState({
-                    ...state,
-                    data: Mainlist,
-                    responseStatus: 'Here is the list of Zomato-Restaurants based on your location'
-                })
-            })
-            .catch(error => {
-                setState({ ...state, responseStatus: "You have not enetered the correct location, please Enter the correct location" })
-            });
-    }, [])
 
     let getLocationLattitude = async (city) => {
         return axios({
@@ -57,13 +20,16 @@ export default function Zomato() {
             }
         })
             .then(response => {
+                // console.log(response)
                 let long = response.data.location_suggestions[0].longitude;
                 let lati = response.data.location_suggestions[0].latitude;
+                let cityId = response.data.location_suggestions[0].city_id;
                 // console.log(long,lati)
                 return (
                     {
                         "long": long,
-                        "lati": lati
+                        "lati": lati,
+                        "cityId": cityId,
                     }
                 );
 
@@ -79,19 +45,20 @@ export default function Zomato() {
 
             await getLocationLattitude(event.target.value)
                 .then(response => {
-
+                    // console.log(response)
                     axios({
                         method: "GET",
-                        url: `https://developers.zomato.com/api/v2.1/geocode?lat=${response.lati}&lon=${response.long}`,
+                        url: `https://developers.zomato.com/api/v2.1/search?entity_id=${response.cityId}&entity_type=city&start=1&count=20&lat=${response.lati}&lon=${response.long}`,
                         headers: {
                             "user-key": "ff4f897b8bc0d97ccd3ed25a6b951fd3",
                             "content-type": "application/json"
                         }
                     })
                         .then(response => {
+                            // console.log(response)
                             let Mainlist = []
                             let simplelist = {}
-                            let nearbyRest = response.data.nearby_restaurants;
+                            let nearbyRest = response.data.restaurants;
                             for (var i of nearbyRest) {
                                 simplelist.name = (i.restaurant.name)
                                 simplelist.average_cost_for_two = (i.restaurant.average_cost_for_two)
@@ -101,13 +68,15 @@ export default function Zomato() {
                                 simplelist.rating_text = (i.restaurant.user_rating.rating_text)
                                 simplelist.featured_image = i.restaurant.featured_image
                                 simplelist.location = i.restaurant.location.locality
+                                simplelist.timing = (i.restaurant.timings).split(',')
+                                simplelist.phone_numbers = (i.restaurant.phone_numbers).split(',')
                                 Mainlist.push(simplelist)
                                 simplelist = {}
                             }
                             setState({
                                 ...state,
                                 data: Mainlist,
-                                responseStatus: 'Here is the list of Zomato-Restaurants based on your location'
+                                responseStatus: `Here is the list of Zomato-Restaurants based on your location`
                             })
                         })
                         .catch(error => {
@@ -146,7 +115,7 @@ export default function Zomato() {
                             <div className="text">
                                 <div className="Row">
                                     <span>CUISINES:</span>
-                                    <span>{rest.cuisines.slice(0, 4)}</span>
+                                    <span>{rest.cuisines.slice(0, 4).join(',')}</span>
                                 </div>
                                 <div className="Row">
                                     <span>COST FOR TWO:</span>
@@ -157,11 +126,19 @@ export default function Zomato() {
                                     <span>{rest.has_online_delivery}</span>
                                 </div>
                                 <div className="Row">
+                                    <span>Timings:</span>
+                                    <span>{rest.timing.slice(0, 2).join(',')}</span>
+                                </div>
+                                <div className="Row">
                                     <span>Rating Text:</span>
                                     <span>{rest.rating_text}</span>
                                 </div>
                             </div>
-                        </article>
+                        </article><hr />
+                        <div className="Row">
+                            <span>Phone Numbers:</span>
+                            <span>{rest.phone_numbers.slice(0, 2).join(',')}</span>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -171,7 +148,8 @@ export default function Zomato() {
 
     return (
         <div>
-            <input className="inputCard" type="text" placeholder="Noida" onKeyPress={searchRestaurant} />
+            <h1>Zomato Restaurants</h1>
+            <input className="inputCard" type="text" placeholder="Search Restaurants based on your location" onKeyPress={searchRestaurant} />
             <div className="inputCards">{state.responseStatus} </div>
             {JSX}
         </div>
